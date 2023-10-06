d23cea4125d42d89a1507d239ffe60b0e51b29e4
HabelKiswanto <104253677+HabelKiswanto@users.noreply.github.com>
2023-10-06 23:41:32 +0700

    Update for booking.py

--- for booking.py
+++ for booking.py
@@ -14,8 +14,8 @@ def is_room_available(cursor, booking_date, booking_time, room_name):
     cursor.execute(query)
     return cursor.fetchone() is None
 
-def has_fine(cursor, student_id):
-    query = f"SELECT fine FROM students WHERE id = {student_id}"
+def has_fine(cursor, nim):
+    query = f"SELECT fine FROM students WHERE nim = {nim}"
     cursor.execute(query)
     result = cursor.fetchone()
     return result and result[0] > 0
@@ -25,7 +25,7 @@ def is_valid_booking_time(booking_time, end_time):
     closing_booking_time = datetime.time(22, 00)
     return opening_booking_time <= booking_time <= closing_booking_time and booking_time <= end_time <= closing_booking_time
 
-def book_room(cursor, student_id, booking_date, booking_time_str, end_time_str, room_name):
+def book_room(cursor, nim, booking_date, booking_time_str, end_time_str, room_name):
     booking_time = datetime.datetime.strptime(booking_time_str, '%H:%M:%S').time()
     end_time = datetime.datetime.strptime(end_time_str, '%H:%M:%S').time()
 
@@ -33,7 +33,7 @@ def book_room(cursor, student_id, booking_date, booking_time_str, end_time_str, 
 
     if not is_valid_booking_time(booking_time, end_time):
         return "Ruangan hanya dapat dipesan antara pukul 7:30 pagi hingga 22:00 malam."
-    if has_fine(cursor, student_id):
+    if has_fine(cursor, nim):
         return "Anda memiliki denda yang belum lunas. Tidak dapat melakukan pemesanan."
     elif not is_room_available(cursor, booking_date, booking_time, room_name):
         return "Ruangan sudah dibooking pada tanggal dan jam tersebut. Silakan coba lagi nanti."
@@ -44,23 +44,22 @@ def book_room(cursor, student_id, booking_date, booking_time_str, end_time_str, 
     elif end_time > datetime.time(22, 0):  
         return "Waktu selesai pemesanan tidak dapat melebihi pukul 22:00."
     else:
-        query = f"INSERT INTO room_bookings (student_id, booking_date, booking_time, end_time, room_name) VALUES ({student_id}, '{booking_date}', '{booking_time}', '{end_time}', '{room_name}')"
+        query = f"INSERT INTO room_bookings (student_id, booking_date, booking_time, end_time, room_name) VALUES ({nim}, '{booking_date}', '{booking_time}', '{end_time}', '{room_name}')"
         cursor.execute(query)
         return "Pemesanan berhasil."
 
 connection = pymysql.connect(host='localhost', user='root', password='', database='perpus calvin')
 cursor = connection.cursor()
 
-student_id = 1
-booking_date = '2023-10-16'
-booking_time = '8:00:00'
-end_time = '10:00:00'  
+nim = 1
+booking_date = '2023-10-11'
+booking_time = '12:00:00'
+end_time = '11:00:00'  
 room_name = 'Ruangan A'
 
-result = book_room(cursor, student_id, booking_date, booking_time, end_time, room_name)
+result = book_room(cursor, nim, booking_date, booking_time, end_time, room_name)
 print(result)
 
 cursor.close()
 connection.close()
 
-#duration between end_time and booking_time
